a1f259d0cf6bd9b813326f32ea6739d1ecd0d7c4
AlexB <a.basalov@a3f.ru>
2024-06-09 20:30:28 +0400

    comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type {Metadata} from "next";
-import {Comic_Neue, Inter, Open_Sans, Roboto} from "next/font/google";
+import {Open_Sans} from "next/font/google";
 import "./globals.css";
 import {Root} from "@/infra/root/root";
 import React from "react";
